693ce86b73202757be294063ba0328f9d6563c03
Fredrik Jonsson <frjo@xdeb.org>
2020-02-14 11:35:28 +0100

    Make changes to existing files as small as possible. Make SITE_NAME set as a enviroment variable (renamed from APP).

--- hypha/public/utils/context_processors.py
+++ hypha/public/utils/context_processors.py
@@ -15,7 +15,7 @@ def global_vars(request):
         'MATOMO_SITEID': settings.MATOMO_SITEID,
     }
 
-    if settings.APP == 'resetnetwork':
+    if settings.SITE_NAME == 'reset':
 
         response['APPLY_SITE'] = ApplyHomePage.objects.first().get_site()
         response['PUBLIC_SITE']: ResetNetworkHomePage.objects.first().get_site()
