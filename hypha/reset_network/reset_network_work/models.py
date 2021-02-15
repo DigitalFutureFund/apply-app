76ae6d4e555053d0bfc91e2ab48a7e2c7d4b1361
Fredrik Jonsson <frjo@xdeb.org>
2021-02-15 11:28:30 +0100

    Add content bottom fields to work.

--- hypha/reset_network/reset_network_work/models.py
+++ hypha/reset_network/reset_network_work/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from modelcluster.fields import ParentalKey
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    InlinePanel,
+    MultiFieldPanel,
+    PageChooserPanel,
+)
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Orderable, Page
 from wagtail.images.edit_handlers import ImageChooserPanel
@@ -52,6 +57,12 @@ class ResetNetworkWorkPage(ResetNetworkBasePage):
 
     region_heading = models.CharField(verbose_name='Heading', max_length=255, null=True, blank=True)
 
+    content_bottom_heading = models.CharField(verbose_name='Heading', max_length=255, blank=True)
+    content_bottom_text = models.TextField(verbose_name='Text', blank=True)
+    content_bottom_link = models.ForeignKey('wagtailcore.Page', verbose_name='Link', null=True, blank=True, related_name='+',
+                                            on_delete=models.PROTECT)
+    content_bottom_link_text = models.CharField(verbose_name='Link text', max_length=255, blank=True)
+
     content_panels = Page.content_panels + [
         MultiFieldPanel([
             FieldPanel('content_heading'),
@@ -65,4 +76,10 @@ class ResetNetworkWorkPage(ResetNetworkBasePage):
             FieldPanel('region_heading'),
             InlinePanel('reset_network_work_region', label='Region', heading='Region')
         ], heading='Content - Regions'),
+        MultiFieldPanel([
+            FieldPanel('content_bottom_heading'),
+            FieldPanel('content_bottom_text'),
+            PageChooserPanel('content_bottom_link'),
+            FieldPanel('content_bottom_link_text')
+        ], heading='Content bottom'),
     ]
