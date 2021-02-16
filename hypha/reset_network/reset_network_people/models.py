6fe1de45e118284bb97f001dc1c8c24dac613cd7
Fredrik Jonsson <frjo@xdeb.org>
2021-02-16 08:49:31 +0100

    Add content bottom to people.

--- hypha/reset_network/reset_network_people/models.py
+++ hypha/reset_network/reset_network_people/models.py
@@ -1,8 +1,14 @@
 from django.db import models
-from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, StreamFieldPanel
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    MultiFieldPanel,
+    PageChooserPanel,
+    StreamFieldPanel,
+)
 from wagtail.core import blocks
 from wagtail.core.fields import RichTextField, StreamField
 from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.snippets.blocks import SnippetChooserBlock
 
 from hypha.reset_network.reset_network_person.models import ResetNetworkPerson
@@ -34,6 +40,14 @@ class ResetNetworkPeoplePage(ResetNetworkBasePage):
         blank=True
     )
 
+    content_bottom_heading = models.CharField(verbose_name='Heading', max_length=255, blank=True)
+    content_bottom_text = models.TextField(verbose_name='Text', blank=True)
+    content_bottom_image = models.ForeignKey('images.CustomImage', verbose_name='Image', null=True, blank=True,
+                                             related_name='+', on_delete=models.SET_NULL)
+    content_bottom_link = models.ForeignKey('wagtailcore.Page', verbose_name='Link', null=True, blank=True, related_name='+',
+                                            on_delete=models.PROTECT)
+    content_bottom_link_text = models.CharField(verbose_name='Link text', max_length=255, blank=True)
+
     content_panels = Page.content_panels + [
         MultiFieldPanel([
             FieldPanel('content_heading'),
@@ -41,4 +55,11 @@ class ResetNetworkPeoplePage(ResetNetworkBasePage):
             FieldPanel('content_long_text'),
         ], heading='Content'),
         StreamFieldPanel('section_1'),
+        MultiFieldPanel([
+            FieldPanel('content_bottom_heading'),
+            FieldPanel('content_bottom_text'),
+            ImageChooserPanel('content_bottom_image'),
+            PageChooserPanel('content_bottom_link'),
+            FieldPanel('content_bottom_link_text')
+        ], heading='Content bottom'),
     ]
